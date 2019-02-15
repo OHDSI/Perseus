6cd94829c187d60f6a2b6c32bcfab1a476dcc335
AlexandrLopoukhov <alexandrlopoukhov@gmail.com>
2019-02-15 18:03:20 +0300

    new json format and code changes for production xml

--- cdm_souffleur/model/xml_writer_new.py
+++ cdm_souffleur/model/xml_writer_new.py
@@ -74,8 +74,15 @@ def get_sql_data(mapping_items):
     all_fields = []
     required_fields = ['source_field', 'sql_field', 'sql_alias']
     mapping_data = mapping_items['mapping']
-    for index, row in mapping_data.iteritems():
-        all_fields += [{x: dic[x] for x in required_fields} for dic in row]
+    condition_data = mapping_items.get('condition')
+
+    if condition_data is not None:
+        result_data = mapping_data + condition_data
+    else:
+        result_data = mapping_data
+
+    for index, row in result_data.iteritems():
+        all_fields += [{k: dic[k] for k in required_fields} for dic in row]
     all_fields_unique = [dict(tuple_map_item) for tuple_map_item in
                          {tuple(map_item.items()) for map_item in all_fields}]
     return pd.DataFrame(all_fields_unique)
@@ -86,6 +93,7 @@ def get_xml(json):
     prepare XML for CDM
     """
     result = ''
+    previous_target_table_name = ''
     # mapping_data = get_mapping(path)
     # lookup_data = get_lookup(path)
     # source_tables = get_source_tables(mapping_data)
@@ -98,67 +106,73 @@ def get_xml(json):
         query_tag = SubElement(query_definition_tag, 'Query')
         sql = prepare_sql(mapping_data, source_table)
         query_tag.text = sql
-        target_tables = pd.unique(
-            mapping_items.loc[mapping_items['source_table']
-                              == source_table,
-                              ['target_table']]
-            ['target_table'].tolist())
+        target_tables = mapping_items.loc[
+            mapping_items['source_table'] == source_table].fillna('')
 
-        for target_table in target_tables:
-            target_table_data = mapping_items.loc[
-                (mapping_items['source_table']
-                 == source_table) & (mapping_items['target_table']
-                                     == target_table)]
-            option = target_table_data.get('option').fillna('').tolist()
-            lookup = target_table_data.get('lookup').fillna('').tolist()
-            mapping = target_table_data.get('mapping').fillna('').tolist()
-            tag_name = convert_underscore_to_camel(target_table)
-            domain_tag = SubElement(query_definition_tag, tag_name)
+        for index, record_data in target_tables.iterrows():
+            option = record_data.get('option')
+            lookup = record_data.get('lookup')
+            mapping = record_data.get('mapping')
+            condition = record_data.get('condition')
+            target_table_name = record_data.get('target_table')
+            tag_name = convert_underscore_to_camel(target_table_name)
+            domain_tag = SubElement(query_definition_tag, tag_name) if \
+                previous_target_table_name != target_table_name else domain_tag
             domain_definition_tag = SubElement(domain_tag,
                                                tag_name + 'Definition')
 
-            for row in option:
-                if row:
-                    options = row['options']
-                    for key, value in options.items():
-                        SubElement(domain_definition_tag, key).text = value
+            if condition is not None:
+                for row in condition:
+                    if row:
+                        condition_text = row['condition']
+                        SubElement(domain_definition_tag,
+                                   'Condition').text = condition_text
 
-            for row in mapping:
-                for item in row:
-                    source_field = item['source_field']
-                    sql_alias = item['sql_alias']
-                    target_field = item['target_field']
+            if option is not None:
+                for row in option:
+                    if row:
+                        options = row['options']
+                        for key, value in options.items():
+                            SubElement(domain_definition_tag, key).text = value
+
+            if mapping is not None:
+                for row in mapping:
+                    source_field = row['source_field']
+                    sql_alias = row['sql_alias']
+                    target_field = row['target_field']
                     v = SubElement(domain_definition_tag, target_field)
                     v.text = sql_alias if sql_alias else source_field
-
-            for row in lookup:
-                if row:
-                    concepts_tag = SubElement(domain_definition_tag,
-                                              'Concepts')
-                    concept_tag = SubElement(concepts_tag, 'Concept')
-                    vocabulary = row['lookup'] if not row else ''
-                    if vocabulary:
-                        concept_id_mapper = SubElement(concept_tag,
-                                                       'ConceptIdMapper')
-                        mapper = SubElement(concept_id_mapper, 'Mapper')
-                        lookup = SubElement(mapper, 'Lookup')
-                        lookup.text = vocabulary
-                    fields = row['fields']
-                    if fields:
-                        fields_tag = SubElement(concept_tag, 'Fields')
-                        # TODO: field is dict with default value and other optional parameters and add validation
-                        # typeId - значение пойдет в ConceptTypeId
-                        # conceptId - значение пойдет в ConceptId
-                        # eventDate - дата из поля будет влиять на маппинг(у концептов есть валидные даты в словаре)
-                        # defaultTypeId - если не смапилось, будет использовано это значение в ConceptTypeId
-                        # defaultConceptId - если не смапилось, будет использовано это значение в ConceptId
-                        # defaultSource - занечение пойдет в SourceValue
-                        # isNullable - запись создасться, даже если в raw был NULL
-                        # for field in fields:
-                        SubElement(fields_tag, 'Field',
-                                   attrib={key: value for
-                                           key, value in
-                                           fields.items()})
+            if lookup is not None:
+                for row in lookup:
+                    if row:
+                        concepts_tag = SubElement(domain_definition_tag,
+                                                  'Concepts')
+                        concept_tag = SubElement(concepts_tag, 'Concept')
+                        vocabulary = row.get('lookup')
+                        if vocabulary:
+                            concept_id_mapper = SubElement(concept_tag,
+                                                           'ConceptIdMapper')
+                            mapper = SubElement(concept_id_mapper, 'Mapper')
+                            lookup = SubElement(mapper, 'Lookup')
+                            lookup.text = vocabulary
+                        fields = row.get('fields')
+                        if fields:
+                            fields_tag = SubElement(concept_tag, 'Fields')
+                            # TODO: field is dict with default value and other optional parameters and add validation
+                            # typeId - значение пойдет в ConceptTypeId
+                            # conceptId - значение пойдет в ConceptId
+                            # eventDate - дата из поля будет влиять на маппинг(у концептов есть валидные даты в словаре)
+                            # defaultTypeId - если не смапилось, будет использовано это значение в ConceptTypeId
+                            # defaultConceptId - если не смапилось, будет использовано это значение в ConceptId
+                            # defaultSource - занечение пойдет в SourceValue
+                            # isNullable - запись создасться, даже если в raw был NULL
+                            # for field in fields:
+                            for field in fields:
+                                SubElement(fields_tag, 'Field',
+                                           attrib={key: value for
+                                                   key, value in
+                                                   field.items()})
+            previous_target_table_name = target_table_name
         # xml = ElementTree(query_definition_tag)
         # xml.write(source_table)
         result += '{} table xml \r\n {} + \r\n'.format(source_table, prettify(
