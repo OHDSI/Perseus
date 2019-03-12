dea8aecec200c4d711f2c3b8427f3d2224df5346
andrey.matyushenko <andrey.matyushenko@arcadia.spb.ru>
2019-03-12 12:57:50 +0300

    Basic routes

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MappingComponent } from './components/layout/mapping/mapping.component';
+import { OverviewComponent } from './components/layout/overview/overview.component';
+import { VocabularyComponent } from './components/layout/vocabulary/vocabulary.component';
+
 
 const routes: Routes = [
- 
+  { path: 'mapping', component: MappingComponent},
+  { path: 'overview', component: OverviewComponent},
+  { path: 'vocabulary', component: VocabularyComponent},
+  { path: '**', component: MappingComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { } 
