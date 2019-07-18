797dcbf81d5a0dccc1cdddd690551b86a2f3e8b9
Max Kilovatiy <maxkvt@gmail.com>
2019-07-18 17:52:55 +0300

    Restore

--- cdm_souffleur/controller/rest_api.py
+++ cdm_souffleur/controller/rest_api.py
@@ -86,5 +86,5 @@ def load_vocabulary_call():
 
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run(host='10.110.1.76', port=5000)
+    app.run(debug=True)
+    #app.run(host='0:0:0:0', port=5000)
