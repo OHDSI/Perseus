1cafa1e52e3dad8318ef2fd820a82b6baeed620a
Max Kilovatiy <maxkvt@gmail.com>
2019-08-28 19:17:42 +0300

    Template bug fix

--- UI/src/app/components/comfy/comfy.component.ts
+++ UI/src/app/components/comfy/comfy.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { StateService } from 'src/app/services/state.service';
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-comfy',
@@ -15,8 +19,12 @@ export class ComfyComponent implements OnInit {
     return this.stateService.state;
   }
 
+  get targetTables(): string[] {
+    return Object.keys(this.target);
+  }
+
   source = [];
-  destination = [];
+  target = {};
   sourceConnectedTo = [];
 
   constructor(
@@ -27,20 +35,32 @@ export class ComfyComponent implements OnInit {
   ngOnInit() {
     this.dataService.initialize().subscribe(_ => {
       this.source = this.state.source.tables.map(table => table.name);
-      this.destination = this.state.target.tables.map(table => [table.name]);
+      this.state.target.tables.map(table => {
+        this.target[table.name] = {};
+        this.target[table.name].name = `idx-${table.name}`;
+        this.target[table.name].data = [table.name];
+      });
+
       this.sourceConnectedTo = this.state.target.tables.map(table => `idx-${table.name}`);
+
       this.busy = false;
     });
   }
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
-      transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     }
   }
 }
