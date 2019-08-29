8969b8219c837420f6a4b26d88dccf83028abaac
Max Kilovatiy <maxkvt@gmail.com>
2019-08-29 16:56:15 +0300

    Hold first element in mapping

--- UI/src/app/components/comfy/comfy.component.ts
+++ UI/src/app/components/comfy/comfy.component.ts
@@ -33,15 +33,20 @@ export class ComfyComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    const prefix = 'target';
+
     this.dataService.initialize().subscribe(_ => {
       this.source = this.state.source.tables.map(table => table.name);
       this.state.target.tables.map(table => {
         this.target[table.name] = {};
-        this.target[table.name].name = `idx-${table.name}`;
+        this.target[table.name].name = `${prefix}-${table.name}`;
+        this.target[table.name].first = table.name;
         this.target[table.name].data = [table.name];
       });
 
-      this.sourceConnectedTo = this.state.target.tables.map(table => `idx-${table.name}`);
+      this.sourceConnectedTo = this.state.target.tables.map(
+        table => `${prefix}-${table.name}`
+      );
 
       this.busy = false;
     });
@@ -61,6 +66,25 @@ export class ComfyComponent implements OnInit {
         event.previousIndex,
         event.currentIndex
       );
+
+      const targetname = event.container.id.split('-')[1];
+      this.setFirstElementAlwaysOnTop(targetname);
+
+      // setTimeout(() => {
+
+      // }, 50);
     }
+  }
+
+  setFirstElementAlwaysOnTop(targetname: string): void {
+    if (!targetname) {
+      return;
+    }
+
+    const { data, first } = this.target[targetname];
+    const index = data.findIndex(value => value === first);
+    const temp = data[0];
+    data[0] = first;
+    data[index] = temp;
   }
 }
