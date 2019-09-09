08124873b44679edf763b1d336c0d76eabb42f2f
AlexandrLopoukhov <alexandrlopoukhov@gmail.com>
2019-09-09 15:47:56 +0300

    find_domain_call  return json object

--- cdm_souffleur/rest_api.py
+++ cdm_souffleur/rest_api.py
@@ -156,8 +156,9 @@ def find_domain_call():
     # TODO what return how to run when init spark?
     column_name = request.args.get('column_name')
     table_name = request.args.get('table_name')
-    start_new_thread(find_domain, (column_name, table_name))
-    return 'OK'
+    finded_codes = find_domain(column_name, table_name).toPandas().to_json(
+        orient='records')
+    return jsonify(finded_codes)
 
 
 @app.route('/load_report')
