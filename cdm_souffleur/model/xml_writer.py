25e3c7c18826c501268f3e8b31b90062986f5ed7
aleksandrlopoukhov <alexander.lopoukhov@arcadia.spb.ru>
2019-10-04 15:25:05 +0300

    change folder name for XML init work on prepare sql for lookup

--- cdm_souffleur/model/xml_writer.py
+++ cdm_souffleur/model/xml_writer.py
@@ -165,6 +165,75 @@ def get_xml(json_):
     return result
 
 
+def get_lookups_sql(cond: dict):
+    for lookup_record in cond:
+        for k, v in lookup_record.items():
+            source_vocabulary_in = ', '.join(v.get('source_vocabulary').get('in'))
+            source_vocabulary_not_in = ', '.join(v.get('source_vocabulary').get('not_in'))
+            target_vocabulary_in = ', '.join(v.get('target_vocabulary').get('in'))
+            target_vocabulary_not_in = ', '.join(v.get('target_vocabulary').get('not_in'))
+            target_concept_class_in = ', '.join(v.get('target_concept_class').get('in'))
+            target_concept_class_not_in = ', '.join(v.get('target_concept_class').get('not_in'))
+            target_domain_in = ', '.join(v.get('target_domain').get('in'))
+            target_domain_not_in = ', '.join(v.get('target_domain').get('not_in'))
+            sql = f"""
+                  Standard as (
+                  SELECT distinct SOURCE_CODE
+                       , TARGET_CONCEPT_ID
+                       , TARGET_DOMAIN_ID
+                       , SOURCE_VALID_START_DATE as VALID_START_DATE
+                       , SOURCE_VALID_END_DATE as VALID_END_DATE
+                       , SOURCE_VOCABULARY_ID
+                  FROM Source_to_Standard
+                  WHERE
+                  SOURCE_VOCABULARY_ID IN {source_vocabulary_in} 
+                  AND SOURCE_VOCABULARY_ID NOT IN {source_vocabulary_not_in}
+                  AND  TARGET_VOCABULARY_ID IN {target_vocabulary_in} 
+                  AND TARGET_VOCABULARY_ID NOT IN {target_vocabulary_not_in}
+                  AND TARGET_CONCEPT_CLASS_ID IN {target_concept_class_in} 
+                  AND TARGET_CONCEPT_CLASS_ID NOT IN {target_concept_class_not_in}
+                  AND TARGET_DOMAIN_ID) IN {target_domain_in} 
+                  AND TARGET_DOMAIN_ID NOT IN {target_domain_not_in}
+                  AND (TARGET_STANDARD_CONCEPT IS NOT NULL or TARGET_STANDARD_CONCEPT != '')
+                  AND (TARGET_INVALID_REASON IS NULL or TARGET_INVALID_REASON = '')), 
+                  Source as (
+                  SELECT distinct SOURCE_CODE
+                       , TARGET_CONCEPT_ID
+                       , SOURCE_VALID_START_DATE
+                       , SOURCE_VALID_END_DATE
+                  FROM Source_to_Source
+                  WHERE
+                  SOURCE_VOCABULARY_ID IN {source_vocabulary_in} 
+                  AND SOURCE_VOCABULARY_ID NOT IN {source_vocabulary_not_in}
+                  AND TARGET_VOCABULARY_ID IN {target_vocabulary_in} 
+                  AND TARGET_VOCABULARY_ID NOT IN {target_vocabulary_not_in}
+                  AND TARGET_CONCEPT_CLASS_ID IN {target_concept_class_in} 
+                  AND TARGET_CONCEPT_CLASS_ID NOT IN {target_concept_class_not_in}
+                  AND TARGET_DOMAIN_ID) IN {target_domain_in} 
+                  AND TARGET_DOMAIN_ID NOT IN {target_domain_not_in}),
+                  S_S as (
+                  select SOURCE_CODE from Standard
+                  union
+                  select SOURCE_CODE from Source)
+ 
+                 select distinct S_S.SOURCE_CODE
+                      , Standard.TARGET_CONCEPT_ID
+                      , Standard.TARGET_DOMAIN_ID
+                      , Standard.VALID_START_DATE
+                      , Standard.VALID_END_DATE
+                      , Standard.SOURCE_VOCABULARY_ID
+                      , Source.TARGET_CONCEPT_ID as SOURCE_TARGET_CONCEPT_ID
+                      , Source.SOURCE_VALID_START_DATE as SOURCE_VALID_START_DATE
+                      , Source.SOURCE_VALID_END_DATE
+                      , ingredient_level.ingredient_concept_id
+                 from S_S
+                 left join Standard on Standard.SOURCE_CODE = S_S.SOURCE_CODE
+                 left join Source on Source.SOURCE_CODE = S_S.SOURCE_CODE
+                 left join ingredient_level on ingredient_level.concept_id = Standard.TARGET_CONCEPT_ID"""
+            print(k)
+            print(sql)
+
+
 def zip_xml():
     try:
         make_archive(
@@ -182,7 +251,7 @@ def delete_generated_xml():
 
 
 if __name__ == '__main__':
-    pass
+    # pass
     # with open('sources/mock_input/ENROLLMENT_DETAIL.json') as file:
     #     data = json.load(file)
     #     print(get_xml(data))
@@ -220,4 +289,56 @@ if __name__ == '__main__':
     # with open('sources/mock_input/mock.json') as file:
     #     data = json.load(file)
     #     print(get_xml(data))
+    from ast import literal_eval
+    condition = literal_eval("""[{"lookup" : 
+{
+  "source_vocabulary": {
+    "in": ["s_voc1", "s_voc2"],
+    "not_in": ["s_voc3", "s_voc4"]
+  }
+  ,
+  "target_vocabulary": {
+    "in": ["t_voc1", "t_voc2"],
+    "not_in": ["t_voc3", "t_voc4"]
+  },
+  "source_concept_class": {
+    "in": ["s_concept1", "s_concept2"],
+    "not_in": ["s_concept3", "s_concept4"]
+  },
+  "target_concept_class": {
+    "in": ["t_concept1", "t_concept2"],
+    "not_in": ["t_concept3", "t_concept4"]
+  },
+  "target_domain": {
+    "in": ["t_domain1", "t_domain2"],
+    "not_in": ["t_domain3", "t_domain4"]
+  }
+}
+},
+{"lookup2" : 
+{
+  "source_vocabulary": {
+    "in": ["s_voc1", "s_voc2"],
+    "not_in": ["s_voc3", "s_voc4"]
+  },
+  "target_vocabulary": {
+    "in": ["t_voc1", "t_voc2"],
+    "not_in": ["t_voc3", "t_voc4"]
+  },
+  "source_concept_class": {
+    "in": ["s_concept1", "s_concept2"],
+    "not_in": ["s_concept3", "s_concept4"]
+  },
+  "target_concept_class": {
+    "in": ["t_concept1", "t_concept2"],
+    "not_in": ["t_concept3", "t_concept4"]
+  },
+  "target_domain": {
+    "in": ["t_domain1", "t_domain2"],
+    "not_in": ["t_domain3", "t_domain4"]
+  }
+}
+}]""")
+    print(condition)
+    get_lookups_sql(condition)
 
