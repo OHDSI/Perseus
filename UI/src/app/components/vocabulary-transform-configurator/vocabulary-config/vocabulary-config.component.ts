43fcc0d2b59687e35bdf8a176be8cee235e788d2
Max Kilovatiy <maxkvt@gmail.com>
2019-10-30 19:16:12 +0300

    Implement concept type table

--- UI/src/app/components/vocabulary-transform-configurator/vocabulary-config/vocabulary-config.component.ts
+++ UI/src/app/components/vocabulary-transform-configurator/vocabulary-config/vocabulary-config.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  Output,
+  EventEmitter
+} from '@angular/core';
 import { ConceptConfig } from '../model/config-concept';
 import { TransformationCondition } from '../model/transformation-config';
 import { VocabularyConfig } from '../model/vocabulary-config';
@@ -8,19 +15,52 @@ import { VocabularyConfig } from '../model/vocabulary-config';
   templateUrl: './vocabulary-config.component.html',
   styleUrls: ['./vocabulary-config.component.scss']
 })
-export class VocabularyConfigComponent implements OnInit {
+export class VocabularyConfigComponent implements OnInit, OnChanges {
   @Input() transformationCondition: TransformationCondition;
+  @Input() selectedSourceFields: string[] = [];
   @Output() configOut = new EventEmitter<TransformationCondition>();
 
   get vocabularyConfig(): VocabularyConfig {
     return this.transformationCondition.vocabularyConfig;
   }
-  constructor() { }
 
-  ngOnInit() {
+  typeConceptItems: TypeConcept[];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngOnChanges() {
+    if (this.selectedSourceFields) {
+      this.typeConceptItems = this.selectedSourceFields.map(fieldName => {
+        const typeconcept: TypeConcept = {
+          sourceFieldName: fieldName,
+          visitTypeIP: 'IP',
+          valueIP: '0',
+          visitTypeOP: 'OP',
+          valueOP: '0',
+          visitTypeER: 'ER',
+          valueER: '0'
+        };
+
+        return typeconcept;
+      });
+    }
   }
 
   outConfigHandler(event: ConceptConfig) {
     this.configOut.emit(this.transformationCondition);
   }
 }
+
+export interface TypeConcept {
+  sourceFieldName: string;
+  visitTypeIP: 'IP';
+  valueIP: string;
+  visitTypeOP: 'OP';
+  valueOP: string;
+  visitTypeER: 'ER';
+  valueER: string;
+}
+
+export type VisitType = 'IP|OP|ER';
