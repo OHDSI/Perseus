b6c137d80ece7bf239275774e3032781624b7f6d
mstupin <mstupin@its.jnj.com>
2020-08-29 03:30:53 +0300

    added lookup(add, get, delete)

--- cdm_souffleur/utils/constants.py
+++ cdm_souffleur/utils/constants.py
@@ -15,5 +15,8 @@ CDM_SCHEMA_PATH = Path('model/sources/CDM/')
 VOCABULARY_DESCRIPTION_PATH = Path('model/sources/VOCABULARY.csv')
 CDM_VERSION_LIST = ['4', '5.0.1', '5.1.0', '5.2.0', '5.3.0', '5.3.1', '5', '6', ]
 UPLOAD_SOURCE_SCHEMA_FOLDER = Path('model/generate/income_schema')
+LOOKUPS_PATH_BASE = Path('model/generate/Lookups')
+PREDEFINED_LOOKUPS_PATH = Path(f'{LOOKUPS_PATH_BASE}/Predefined')
+INCOME_LOOKUPS_PATH = Path(f'{LOOKUPS_PATH_BASE}/Income')
 
 
