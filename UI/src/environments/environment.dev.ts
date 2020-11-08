e95d2b0dfc9d2269c548dc68f4165466b7f13c96
ARCADIA\maria.dolotova <maria.dolotova@arcadia.spb.ru>
2020-11-08 20:02:40 +0300

    correction in backend url for dev environment

--- UI/src/environments/environment.dev.ts
+++ UI/src/environments/environment.dev.ts
@@ -12,7 +12,7 @@ const CONCEPT_TABLES = [
 
 export const environment = {
     production: false,
-    url: 'http://10.110.1.7:5001/dev/api',
+    url: 'http://10.110.1.7/dev/api',
     conceptTables: CONCEPT_TABLES,
     config: 'dev'
 };
