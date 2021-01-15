841491fc7c6c50856d4ad3a7415b426bbc3fdac0
Chudakov Matvey <morganrush@yandex.ru>
2021-01-15 18:23:58 +0300

    fixed grouped fields in word report

--- UI/src/app/services/report/mapping-util.ts
+++ UI/src/app/services/report/mapping-util.ts
@@ -2,11 +2,11 @@ import { MappingNode } from '../../models/mapping';
 
 export function parseMappingNodesByGroups(mappingNodes: MappingNode[]): MappingNode[] {
   const groupReducer = (accumulator: {[key: string]: MappingNode}, currentValue: MappingNode) => {
-    const currentNode = accumulator[currentValue.groupName];
+    const currentNode = accumulator[currentValue.groupName + currentValue.target_field];
     if (currentNode) {
       currentNode.source_field = `${currentNode.source_field}\n${currentValue.source_field}`;
     } else {
-      accumulator[currentValue.groupName] = currentValue;
+      accumulator[currentValue.groupName + currentValue.target_field] = {...currentValue}; // Copy object
     }
 
     return accumulator;
