1daa5ae37883aa5974f54306ab0fb04be281fc64
Chudakov Matvey <morganrush@yandex.ru>
2021-02-16 14:30:23 +0300

    vocabulary db: fixed init sql

--- vocabulary_database/init.sql
+++ vocabulary_database/init.sql
@@ -4,7 +4,7 @@ create schema "vocabulary";
 
 
 --- tables
-CREATE TABLE "vocabulary.concept"
+CREATE TABLE "vocabulary"."concept"
 (
    concept_id        int            NOT NULL,
    concept_name      text           NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE "vocabulary.concept"
 
 
 
-CREATE TABLE "vocabulary.concept_ancestor"
+CREATE TABLE "vocabulary"."concept_ancestor"
 (
    ancestor_concept_id       int   NOT NULL,
    descendant_concept_id     int   NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE "vocabulary.concept_ancestor"
 
 
 
-CREATE TABLE "vocabulary.concept_class"
+CREATE TABLE "vocabulary"."concept_class"
 (
    concept_class_id          varchar(20)    NOT NULL,
    concept_class_name        varchar(255)   NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE "vocabulary.concept_class"
 
 
 
-CREATE TABLE "vocabulary.concept_relationship"
+CREATE TABLE "vocabulary"."concept_relationship"
 (
    concept_id_1      int           NOT NULL,
    concept_id_2      int           NOT NULL,
@@ -51,7 +51,7 @@ CREATE TABLE "vocabulary.concept_relationship"
 
 
 
-CREATE TABLE "vocabulary.concept_synonym"
+CREATE TABLE "vocabulary"."concept_synonym"
 (
    concept_id            int             NOT NULL,
    concept_synonym_name  varchar(1000)   NOT NULL,
@@ -60,7 +60,7 @@ CREATE TABLE "vocabulary.concept_synonym"
 
 
 
-CREATE TABLE "vocabulary.domain"
+CREATE TABLE "vocabulary"."domain"
 (
    domain_id          varchar(20)    NOT NULL,
    domain_name        varchar(255)   NOT NULL,
@@ -69,7 +69,7 @@ CREATE TABLE "vocabulary.domain"
 
 
 
-CREATE TABLE "vocabulary.drug_strength"
+CREATE TABLE "vocabulary"."drug_strength"
 (
    drug_concept_id              int          NOT NULL,
    ingredient_concept_id        int          NOT NULL,
@@ -87,7 +87,7 @@ CREATE TABLE "vocabulary.drug_strength"
 
 
 
-CREATE TABLE "vocabulary.relationship"
+CREATE TABLE "vocabulary"."relationship"
 (
    relationship_id          varchar(20)    NOT NULL,
    relationship_name        varchar(255)   NOT NULL,
@@ -99,7 +99,7 @@ CREATE TABLE "vocabulary.relationship"
 
 
 
-CREATE TABLE "vocabulary.source_to_concept_map"
+CREATE TABLE "vocabulary"."source_to_concept_map"
 (
    source_code              varchar(255)    NOT NULL,
    source_concept_id        int            NOT NULL,
@@ -114,7 +114,7 @@ CREATE TABLE "vocabulary.source_to_concept_map"
 
 
 
-CREATE TABLE "vocabulary.vocabulary"
+CREATE TABLE "vocabulary"."vocabulary"
 (
    vocabulary_id          varchar(20)    NOT NULL,
    vocabulary_name        varchar(255)   NOT NULL,
@@ -126,12 +126,12 @@ CREATE TABLE "vocabulary.vocabulary"
 
 
 --- copy
-COPY "vocabulary.concept" FROM '/tmp/vocabulary/CONCEPT.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
-COPY "vocabulary.concept_ancestor" FROM '/tmp/vocabulary/CONCEPT_ANCESTOR.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
-COPY "vocabulary.concept_class" FROM '/tmp/vocabulary/CONCEPT_CLASS.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
-COPY "vocabulary.concept_relationship" FROM '/tmp/vocabulary/CONCEPT_RELATIONSHIP.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
-COPY "vocabulary.concept_synonym" FROM '/tmp/vocabulary/CONCEPT_SYNONYM.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
-COPY "vocabulary.domain" FROM '/tmp/vocabulary/DOMAIN.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
-COPY "vocabulary.drug_strength" FROM '/tmp/vocabulary/DRUG_STRENGTH.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
-COPY "vocabulary.relationship" FROM '/tmp/vocabulary/RELATIONSHIP.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
-COPY "vocabulary.vocabulary" FROM '/tmp/vocabulary/VOCABULARY.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
+COPY "vocabulary"."concept" FROM '/tmp/vocabulary/CONCEPT.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
+COPY "vocabulary"."concept_ancestor" FROM '/tmp/vocabulary/CONCEPT_ANCESTOR.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
+COPY "vocabulary"."concept_class" FROM '/tmp/vocabulary/CONCEPT_CLASS.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
+COPY "vocabulary"."concept_relationship" FROM '/tmp/vocabulary/CONCEPT_RELATIONSHIP.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
+COPY "vocabulary"."concept_synonym" FROM '/tmp/vocabulary/CONCEPT_SYNONYM.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
+COPY "vocabulary"."domain" FROM '/tmp/vocabulary/DOMAIN.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
+COPY "vocabulary"."drug_strength" FROM '/tmp/vocabulary/DRUG_STRENGTH.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
+COPY "vocabulary"."relationship" FROM '/tmp/vocabulary/RELATIONSHIP.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
+COPY "vocabulary"."vocabulary" FROM '/tmp/vocabulary/VOCABULARY.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
