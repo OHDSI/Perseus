25e41b106a84c405018d4628a1663d52e17b7ffe
Chudakov Matvey <matvey.chudakov@arcadia.spb.ru>
2021-02-17 13:24:20 +0300

    vocabulary db: changed init sql script

--- vocabulary_database/init.sql
+++ vocabulary_database/init.sql
@@ -1,13 +1,18 @@
---- schema
+--- schema ---
 create schema "vocabulary";
 
---- user
+
+
+--- user ---
 CREATE USER cdm_builder WITH PASSWORD 'N7jscuS3ca';
 
 GRANT USAGE ON SCHEMA vocabulary TO cdm_builder;
 GRANT SELECT ON ALL TABLES IN SCHEMA vocabulary TO cdm_builder;
 
---- tables
+
+
+--- tables ---
+--- concept
 CREATE TABLE "vocabulary"."concept"
 (
    concept_id        int            NOT NULL,
@@ -22,6 +27,22 @@ CREATE TABLE "vocabulary"."concept"
    invalid_reason    varchar(1)
 );
 
+COPY "vocabulary"."concept" FROM '/tmp/vocabulary/CONCEPT.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
+
+ALTER TABLE "vocabulary"."concept" ADD CONSTRAINT xpk_concept PRIMARY KEY (concept_id);
+
+CREATE UNIQUE INDEX idx_concept_concept_id ON "vocabulary"."concept" (concept_id ASC);
+CLUSTER "vocabulary"."concept" USING idx_concept_concept_id ;
+CREATE INDEX idx_concept_code ON "vocabulary"."concept" (concept_code ASC);
+CREATE INDEX idx_concept_vocabluary_id ON "vocabulary"."concept" (vocabulary_id ASC);
+CREATE INDEX idx_concept_domain_id ON "vocabulary"."concept" (domain_id ASC);
+CREATE INDEX idx_concept_class_id ON "vocabulary"."concept" (concept_class_id ASC);
+
+VACUUM FULL "vocabulary"."concept";
+
+
+
+--- concept_ancestor
 CREATE TABLE "vocabulary"."concept_ancestor"
 (
    ancestor_concept_id       int   NOT NULL,
@@ -30,6 +51,19 @@ CREATE TABLE "vocabulary"."concept_ancestor"
    max_levels_of_separation  int   NOT NULL
 );
 
+COPY "vocabulary"."concept_ancestor" FROM '/tmp/vocabulary/CONCEPT_ANCESTOR.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
+
+ALTER TABLE "vocabulary"."concept_ancestor" ADD CONSTRAINT xpk_concept_ancestor PRIMARY KEY (ancestor_concept_id,descendant_concept_id);
+
+CREATE INDEX idx_concept_ancestor_id_1 ON "vocabulary"."concept_ancestor" (ancestor_concept_id ASC);
+CLUSTER "vocabulary"."concept_ancestor" USING idx_concept_ancestor_id_1;
+CREATE INDEX idx_concept_ancestor_id_2 ON "vocabulary"."concept_ancestor" (descendant_concept_id ASC);
+
+VACUUM FULL "vocabulary"."concept_ancestor";
+
+
+
+--- concept_class
 CREATE TABLE "vocabulary"."concept_class"
 (
    concept_class_id          varchar(20)    NOT NULL,
@@ -37,6 +71,18 @@ CREATE TABLE "vocabulary"."concept_class"
    concept_class_concept_id  int            NOT NULL
 );
 
+COPY "vocabulary"."concept_class" FROM '/tmp/vocabulary/CONCEPT_CLASS.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
+
+ALTER TABLE "vocabulary"."concept_class" ADD CONSTRAINT xpk_concept_class PRIMARY KEY (concept_class_id);
+
+CREATE UNIQUE INDEX idx_concept_class_class_id ON "vocabulary"."concept_class" (concept_class_id ASC);
+CLUSTER "vocabulary"."concept_class" USING idx_concept_class_class_id ;
+
+VACUUM FULL "vocabulary"."concept_class";
+
+
+
+--- concept_relationship
 CREATE TABLE "vocabulary"."concept_relationship"
 (
    concept_id_1      int           NOT NULL,
@@ -47,6 +93,18 @@ CREATE TABLE "vocabulary"."concept_relationship"
    invalid_reason    varchar(1)
 );
 
+COPY "vocabulary"."concept_relationship" FROM '/tmp/vocabulary/CONCEPT_RELATIONSHIP.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
+
+ALTER TABLE "vocabulary"."concept_relationship" ADD CONSTRAINT xpk_concept_relationship PRIMARY KEY (concept_id_1,concept_id_2,relationship_id);
+
+CREATE INDEX idx_concept_relationship_id_1 ON "vocabulary"."concept_relationship" (concept_id_1 ASC);
+CREATE INDEX idx_concept_relationship_id_2 ON "vocabulary"."concept_relationship" (concept_id_2 ASC);
+CREATE INDEX idx_concept_relationship_id_3 ON "vocabulary"."concept_relationship" (relationship_id ASC);
+
+VACUUM FULL "vocabulary"."concept_relationship";
+
+
+--- concept_synonym
 CREATE TABLE "vocabulary"."concept_synonym"
 (
    concept_id            int             NOT NULL,
@@ -54,6 +112,16 @@ CREATE TABLE "vocabulary"."concept_synonym"
    language_concept_id   int             NOT NULL
 );
 
+COPY "vocabulary"."concept_synonym" FROM '/tmp/vocabulary/CONCEPT_SYNONYM.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
+
+CREATE INDEX idx_concept_synonym_id ON "vocabulary"."concept_synonym" (concept_id ASC);
+CLUSTER "vocabulary"."concept_synonym" USING idx_concept_synonym_id;
+
+VACUUM FULL "vocabulary"."concept_synonym";
+
+
+
+--- domain
 CREATE TABLE "vocabulary"."domain"
 (
    domain_id          varchar(20)    NOT NULL,
@@ -61,6 +129,18 @@ CREATE TABLE "vocabulary"."domain"
    domain_concept_id  int            NOT NULL
 );
 
+COPY "vocabulary"."domain" FROM '/tmp/vocabulary/DOMAIN.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
+
+ALTER TABLE "vocabulary"."domain" ADD CONSTRAINT xpk_domain PRIMARY KEY (domain_id);
+
+CREATE UNIQUE INDEX idx_domain_domain_id ON "vocabulary"."domain" (domain_id ASC);
+CLUSTER "vocabulary"."domain" USING idx_domain_domain_id ;
+
+VACUUM FULL "vocabulary"."domain";
+
+
+
+--- drug_strength
 CREATE TABLE "vocabulary"."drug_strength"
 (
    drug_concept_id              int          NOT NULL,
@@ -77,6 +157,19 @@ CREATE TABLE "vocabulary"."drug_strength"
    invalid_reason               varchar(1)
 );
 
+COPY "vocabulary"."drug_strength" FROM '/tmp/vocabulary/DRUG_STRENGTH.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
+
+ALTER TABLE "vocabulary"."drug_strength" ADD CONSTRAINT xpk_drug_strength PRIMARY KEY (drug_concept_id, ingredient_concept_id);
+
+CREATE INDEX idx_drug_strength_id_1 ON "vocabulary"."drug_strength" (drug_concept_id ASC);
+CLUSTER "vocabulary"."drug_strength"  USING idx_drug_strength_id_1;
+CREATE INDEX idx_drug_strength_id_2 ON "vocabulary"."drug_strength" (ingredient_concept_id ASC);
+
+VACUUM FULL "vocabulary"."drug_strength";
+
+
+
+--- relationship
 CREATE TABLE "vocabulary"."relationship"
 (
    relationship_id          varchar(20)    NOT NULL,
@@ -87,6 +180,18 @@ CREATE TABLE "vocabulary"."relationship"
    relationship_concept_id  int            NOT NULL
 );
 
+COPY "vocabulary"."relationship" FROM '/tmp/vocabulary/RELATIONSHIP.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
+
+ALTER TABLE "vocabulary"."relationship" ADD CONSTRAINT xpk_relationship PRIMARY KEY (relationship_id);
+
+CREATE UNIQUE INDEX idx_relationship_rel_id ON "vocabulary"."relationship" (relationship_id ASC);
+CLUSTER "vocabulary"."relationship" USING idx_relationship_rel_id;
+
+VACUUM FULL "vocabulary"."relationship";
+
+
+
+--- source_to_concept_map
 CREATE TABLE "vocabulary"."source_to_concept_map"
 (
    source_code              varchar(255)    NOT NULL,
@@ -100,6 +205,19 @@ CREATE TABLE "vocabulary"."source_to_concept_map"
    invalid_reason           varchar(1)
 );
 
+-- ALTER TABLE "vocabulary"."source_to_concept_map" ADD CONSTRAINT xpk_source_to_concept_map PRIMARY KEY (source_vocabulary_id,target_concept_id,source_code,valid_end_date);
+
+CREATE INDEX idx_source_to_concept_map_id_3 ON "vocabulary"."source_to_concept_map" (target_concept_id ASC);
+CLUSTER "vocabulary"."source_to_concept_map"  USING idx_source_to_concept_map_id_3;
+CREATE INDEX idx_source_to_concept_map_id_1 ON "vocabulary"."source_to_concept_map" (source_vocabulary_id ASC);
+CREATE INDEX idx_source_to_concept_map_id_2 ON "vocabulary"."source_to_concept_map" (target_vocabulary_id ASC);
+CREATE INDEX idx_source_to_concept_map_code ON "vocabulary"."source_to_concept_map" (source_code ASC);
+
+VACUUM FULL "vocabulary"."source_to_concept_map";
+
+
+
+--- vocabulary
 CREATE TABLE "vocabulary"."vocabulary"
 (
    vocabulary_id          varchar(20)    NOT NULL,
@@ -109,76 +227,9 @@ CREATE TABLE "vocabulary"."vocabulary"
    vocabulary_concept_id  int            NOT NULL
 );
 
---- copy
-COPY "vocabulary"."concept" FROM '/tmp/vocabulary/CONCEPT.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
-COPY "vocabulary"."concept_ancestor" FROM '/tmp/vocabulary/CONCEPT_ANCESTOR.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
-COPY "vocabulary"."concept_class" FROM '/tmp/vocabulary/CONCEPT_CLASS.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
-COPY "vocabulary"."concept_relationship" FROM '/tmp/vocabulary/CONCEPT_RELATIONSHIP.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
-COPY "vocabulary"."concept_synonym" FROM '/tmp/vocabulary/CONCEPT_SYNONYM.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
-COPY "vocabulary"."domain" FROM '/tmp/vocabulary/DOMAIN.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
-COPY "vocabulary"."drug_strength" FROM '/tmp/vocabulary/DRUG_STRENGTH.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
-COPY "vocabulary"."relationship" FROM '/tmp/vocabulary/RELATIONSHIP.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
 COPY "vocabulary"."vocabulary" FROM '/tmp/vocabulary/VOCABULARY.csv' WITH (FORMAT CSV, DELIMITER E'\t', HEADER TRUE, QUOTE E'\b');
-
---- indexes
-ALTER TABLE "vocabulary"."concept" ADD CONSTRAINT xpk_concept PRIMARY KEY (concept_id);
-ALTER TABLE "vocabulary"."vocabulary" ADD CONSTRAINT xpk_vocabulary PRIMARY KEY (vocabulary_id);
-ALTER TABLE "vocabulary"."domain" ADD CONSTRAINT xpk_domain PRIMARY KEY (domain_id);
-ALTER TABLE "vocabulary"."concept_class" ADD CONSTRAINT xpk_concept_class PRIMARY KEY (concept_class_id);
-ALTER TABLE "vocabulary"."concept_relationship" ADD CONSTRAINT xpk_concept_relationship PRIMARY KEY (concept_id_1,concept_id_2,relationship_id);
-ALTER TABLE "vocabulary"."relationship" ADD CONSTRAINT xpk_relationship PRIMARY KEY (relationship_id);
-ALTER TABLE "vocabulary"."concept_ancestor" ADD CONSTRAINT xpk_concept_ancestor PRIMARY KEY (ancestor_concept_id,descendant_concept_id);
--- ALTER TABLE "vocabulary"."source_to_concept_map" ADD CONSTRAINT xpk_source_to_concept_map PRIMARY KEY (source_vocabulary_id,target_concept_id,source_code,valid_end_date);
-ALTER TABLE "vocabulary"."drug_strength" ADD CONSTRAINT xpk_drug_strength PRIMARY KEY (drug_concept_id, ingredient_concept_id);
-
-CREATE UNIQUE INDEX idx_concept_concept_id ON "vocabulary"."concept" (concept_id ASC);
-CLUSTER "vocabulary"."concept" USING idx_concept_concept_id ;
-CREATE INDEX idx_concept_code ON "vocabulary"."concept" (concept_code ASC);
-CREATE INDEX idx_concept_vocabluary_id ON "vocabulary"."concept" (vocabulary_id ASC);
-CREATE INDEX idx_concept_domain_id ON "vocabulary"."concept" (domain_id ASC);
-CREATE INDEX idx_concept_class_id ON "vocabulary"."concept" (concept_class_id ASC);
 
 CREATE UNIQUE INDEX idx_vocabulary_vocabulary_id ON "vocabulary"."vocabulary" (vocabulary_id ASC);
 CLUSTER "vocabulary"."vocabulary" USING idx_vocabulary_vocabulary_id ;
 
-CREATE UNIQUE INDEX idx_domain_domain_id ON "vocabulary"."domain" (domain_id ASC);
-CLUSTER "vocabulary"."domain" USING idx_domain_domain_id ;
-
-CREATE UNIQUE INDEX idx_concept_class_class_id ON "vocabulary"."concept_class" (concept_class_id ASC);
-CLUSTER "vocabulary"."concept_class" USING idx_concept_class_class_id ;
-
-CREATE INDEX idx_concept_relationship_id_1 ON "vocabulary"."concept_relationship" (concept_id_1 ASC);
-CREATE INDEX idx_concept_relationship_id_2 ON "vocabulary"."concept_relationship" (concept_id_2 ASC);
-CREATE INDEX idx_concept_relationship_id_3 ON "vocabulary"."concept_relationship" (relationship_id ASC);
-
-CREATE UNIQUE INDEX idx_relationship_rel_id ON "vocabulary"."relationship" (relationship_id ASC);
-CLUSTER "vocabulary"."relationship" USING idx_relationship_rel_id;
-
-CREATE INDEX idx_concept_synonym_id ON "vocabulary"."concept_synonym" (concept_id ASC);
-CLUSTER "vocabulary"."concept_synonym" USING idx_concept_synonym_id;
-
-CREATE INDEX idx_concept_ancestor_id_1 ON "vocabulary"."concept_ancestor" (ancestor_concept_id ASC);
-CLUSTER "vocabulary"."concept_ancestor" USING idx_concept_ancestor_id_1;
-CREATE INDEX idx_concept_ancestor_id_2 ON "vocabulary"."concept_ancestor" (descendant_concept_id ASC);
-
-CREATE INDEX idx_source_to_concept_map_id_3 ON "vocabulary"."source_to_concept_map" (target_concept_id ASC);
-CLUSTER "vocabulary"."source_to_concept_map"  USING idx_source_to_concept_map_id_3;
-CREATE INDEX idx_source_to_concept_map_id_1 ON "vocabulary"."source_to_concept_map" (source_vocabulary_id ASC);
-CREATE INDEX idx_source_to_concept_map_id_2 ON "vocabulary"."source_to_concept_map" (target_vocabulary_id ASC);
-CREATE INDEX idx_source_to_concept_map_code ON "vocabulary"."source_to_concept_map" (source_code ASC);
-
-CREATE INDEX idx_drug_strength_id_1 ON "vocabulary"."drug_strength" (drug_concept_id ASC);
-CLUSTER "vocabulary"."drug_strength"  USING idx_drug_strength_id_1;
-CREATE INDEX idx_drug_strength_id_2 ON "vocabulary"."drug_strength" (ingredient_concept_id ASC);
-
---- Vacuum analyze
-VACUUM FULL "vocabulary"."concept";
-VACUUM FULL "vocabulary"."concept_ancestor";
-VACUUM FULL "vocabulary"."concept_class";
-VACUUM FULL "vocabulary"."concept_relationship";
-VACUUM FULL "vocabulary"."concept_synonym";
-VACUUM FULL "vocabulary"."domain";
-VACUUM FULL "vocabulary"."drug_strength";
-VACUUM FULL "vocabulary"."relationship";
-VACUUM FULL "vocabulary"."source_to_concept_map";
 VACUUM FULL "vocabulary"."vocabulary";
