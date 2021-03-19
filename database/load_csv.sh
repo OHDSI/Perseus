437bc464d72e070707dbeeb866f959bc4e36487c
Chudakov Matvey <matvey.chudakov@arcadia.spb.ru>
2021-03-19 16:26:11 +0300

    changed dictionary link

--- database/load_csv.sh
+++ database/load_csv.sh
@@ -2,6 +2,6 @@ apt-get update -y
 apt-get install unzip wget -y
 
 cd tmp
-wget -O vocabulary.zip https://athena.ohdsi.org/api/v1/vocabularies/zip/73484d62-6688-4abe-a3e8-55a0f204c6cc
+wget -O vocabulary.zip https://athena.ohdsi.org/api/v1/vocabularies/zip/0f85a705-2000-4934-8b1a-5dcb77492071
 unzip 'vocabulary.zip' -d vocabulary
 rm vocabulary.zip
