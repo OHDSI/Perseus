97cf2f60ead6087eb362bc8b5169eabf991deea6
ARCADIA\maria.dolotova <maria.dolotova@arcadia.spb.ru>
2021-03-26 15:15:46 +0300

    removed async for method afterOpenMapping

--- UI/src/app/cdm/comfy/comfy.component.ts
+++ UI/src/app/cdm/comfy/comfy.component.ts
@@ -346,7 +346,7 @@ export class ComfyComponent extends BaseComponent implements OnInit, AfterViewIn
     return this.dataService.getTargetData(version).subscribe();
   }
 
-  async afterOpenMapping(event?: any) {
+  afterOpenMapping(event?: any) {
     if (!event || event.index !== 0)
       this.router.navigate([`${mainPageRouter}/mapping`], { queryParams: event, skipLocationChange: true});
   }
