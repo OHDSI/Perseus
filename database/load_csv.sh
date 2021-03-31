00f4cf5ed6512446309a9b09e249355fd103bdc4
Arcuser <arcuser@jnjcdmu1.arcadia.intranet>
2021-03-31 11:03:47 +0300

    vocabulary url is moved to container' parameters

--- database/load_csv.sh
+++ database/load_csv.sh
@@ -2,6 +2,6 @@ apt-get update -y
 apt-get install unzip wget -y
 
 cd tmp
-wget -O vocabulary.zip https://athena.ohdsi.org/api/v1/vocabularies/zip/0f85a705-2000-4934-8b1a-5dcb77492071
+wget -O vocabulary.zip $VOC_URL
 unzip 'vocabulary.zip' -d vocabulary
 rm vocabulary.zip
