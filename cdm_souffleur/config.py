9580685495d2dbd5d2b748604115820b9ba5b095
ARCADIA\maria.dolotova <maria.dolotova@arcadia.spb.ru>
2021-04-13 15:03:56 +0300

    added variables to dev config

--- cdm_souffleur/config.py
+++ cdm_souffleur/config.py
@@ -23,3 +23,8 @@ class DevelopmentConfig:
     DB_HOST = 'localhost'
     DB_PORT = 5431
     SECRET_KEY = "\xac\xe3\xc1\x8ft\x8d\x7fM\x82\x18\xbeM\xc7k*\xd5]\xae(J\xe6}\xe84"
+    SMTP_SERVER = 'mail.arcadialab.ru'
+    SMTP_PORT = 587
+    SMTP_EMAIL = 'perseus_test@arcadialab.ru'
+    SMTP_USER = 'perseus_test'
+    SMTP_PWD = 'PT4TYxou@?cH8J'
