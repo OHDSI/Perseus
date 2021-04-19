fab5c938aa5618bc0f37888b6070b08ef6bb7c2e
Chudakov Matvey <morganrush@yandex.ru>
2021-04-19 12:08:17 +0300

    fix logout dialog

--- UI/src/app/toolbar/toolbar.component.ts
+++ UI/src/app/toolbar/toolbar.component.ts
@@ -217,7 +217,7 @@ export class ToolbarComponent extends BaseComponent implements OnInit, OnDestroy
     this.matDialog.open(LogoutComponent, {
       width: '301',
       height: '220',
-      panelClass: 'white-rabbit-dialog'
+      panelClass: 'perseus-dialog'
     });
   }
 
