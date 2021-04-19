cb01f2685838c505d49bc243e01851946e5d87eb
Chudakov Matvey <morganrush@yandex.ru>
2021-04-19 12:26:58 +0300

    updated email template

--- UI/src/app/common/cdm-common.module.ts
+++ UI/src/app/common/cdm-common.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { PrettyNamePipe } from './pipes/pretty-name.pipe';
 import { TypeToIconPipe } from './pipes/type-to-icon.pipe';
 import { SearchByNameComponent } from './search-by-name/search-by-name.component';
-import { CdmCustomMaterialModule } from '../../material/cdm-custom-material.module';
+import { CdmCustomMaterialModule } from '../material/cdm-custom-material.module';
 import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 import { CloseDialogButtonComponent } from './close-dialog-button/close-dialog-button.component';
 
