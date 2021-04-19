7fcdd98877d0a34b5fe091d8293ef275f36cdedd
Chudakov Matvey <morganrush@yandex.ru>
2021-04-19 12:18:09 +0300

    update logo

--- UI/src/app/auth/auxiliary/auth-logo/auth-logo.component.ts
+++ UI/src/app/auth/auxiliary/auth-logo/auth-logo.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-auth-logo',
   templateUrl: './auth-logo.component.html',
+  styles: [
+    'svg {width: 196px; height: 26px}'
+  ]
 })
 export class AuthLogoComponent {
 }
