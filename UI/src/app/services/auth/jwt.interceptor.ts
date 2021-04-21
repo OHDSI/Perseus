a70998b4ec4c75073735f351f17d31fc0bac78a1
Chudakov Matvey <morganrush@yandex.ru>
2021-04-21 16:35:26 +0300

    fixed refresh token handling

--- UI/src/app/services/auth/jwt.interceptor.ts
+++ UI/src/app/services/auth/jwt.interceptor.ts
@@ -1,14 +1,18 @@
 import { Inject, Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
 import { authInjector } from './auth-injector';
 import { AuthService } from './auth.service';
-import { catchError, switchMap } from 'rxjs/operators';
+import { catchError, filter, finalize, switchMap, take } from 'rxjs/operators';
 import { loginRouter } from '../../app.constants';
 import { Router } from '@angular/router';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+
+  private isRefreshing = false;
+
+  private refreshToken$ = new BehaviorSubject<string>(null);
 
   constructor(@Inject(authInjector) private authService: AuthService,
               private router: Router) {}
@@ -39,14 +43,31 @@ export class JwtInterceptor implements HttpInterceptor {
   }
 
   private handle401Error(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const {email, refresh_token} = this.authService.user
-    return this.authService.refreshToken(email, refresh_token)
-      .pipe(
-        switchMap(() => next.handle(this.addAuthorizationHeader(request))),
-        catchError(error => {
-          this.router.navigateByUrl(loginRouter)
-          throw error
-        })
-      )
+    if (this.isRefreshing) {
+      return this.refreshToken$
+        .pipe(
+          filter(token => token !== null),
+          take(1),
+          switchMap(() => next.handle(this.addAuthorizationHeader(request)))
+        )
+    } else {
+      this.isRefreshing = true;
+      this.refreshToken$.next(null);
+
+      const {email, refresh_token} = this.authService.user
+      return this.authService.refreshToken(email, refresh_token)
+        .pipe(
+          catchError(error => {
+            this.refreshToken$.error(error)
+            this.router.navigateByUrl(loginRouter)
+            throw error
+          }),
+          switchMap(user => {
+            this.refreshToken$.next(user.refresh_token)
+            return next.handle(this.addAuthorizationHeader(request))
+          }),
+          finalize(() => this.isRefreshing = false)
+        )
+    }
   }
 }
