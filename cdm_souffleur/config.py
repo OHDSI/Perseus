6007753202a3e7f52515eeedb678b191c2cbce91
Maria Dolotova <mdoloto1@its.jnj.com>
2021-04-29 15:20:20 +0300

    corrected config

--- cdm_souffleur/config.py
+++ cdm_souffleur/config.py
@@ -4,7 +4,7 @@ class DefaultConfig:
     DB_NAME = 'cdm_souffleur'
     DB_USER = 'postgres'
     DB_PASSWORD = '5eC_DkMr^3'
-    DB_HOST = '10.110.1.7'
+    DB_HOST = 'localhost'
     DB_PORT = 5431
     SECRET_KEY = "\xac\xe3\xc1\x8ft\x8d\x7fM\x82\x18\xbeM\xc7k*\xd5]\xae(J\xe6}\xe84"
     SMTP_SERVER = 'mail.arcadialab.ru'
