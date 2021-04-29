4401f8cd4581360009e9ff5c740cc4e4b4fca09d
Chudakov Matvey <morganrush@yandex.ru>
2021-04-29 17:26:20 +0300

    refactored

--- UI/src/environments/environment.ts
+++ UI/src/environments/environment.ts
@@ -23,10 +23,10 @@ export const environment = {
 };
 
 /*
- * For easier debugging in development mode, you can import-vocabulary the following file
+ * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
- * This import-vocabulary should be commented out in production mode because it will have a negative impact
+ * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import-vocabulary 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
