79e15e7b84e8e9c1b8af80adf2d09699975932cb
Maria Dolotova <mdoloto1@its.jnj.com>
2021-07-27 16:28:54 +0300

    corrected config for staging

--- cdm_souffleur/config.py
+++ cdm_souffleur/config.py
@@ -56,21 +56,3 @@ class StagingConfig:
     SOLR_HOST = 'localhost'
     SOLR_PORT = '8983'
 
-
-class StagingConfig:
-    CDM_SOUFFLEUR_PREFIX = ''
-    CDM_SOUFFLEUR_PORT = 5000
-    DB_NAME = 'cdm_souffleur'
-    DB_USER = 'postgres'
-    DB_PASSWORD = '5eC_DkMr^3'
-    DB_HOST = 'localhost'
-    DB_PORT = 5431
-    SECRET_KEY = "\xac\xe3\xc1\x8ft\x8d\x7fM\x82\x18\xbeM\xc7k*\xd5]\xae(J\xe6}\xe84"
-    SMTP_SERVER = 'mail.arcadialab.ru'
-    SMTP_PORT = 587
-    SMTP_EMAIL = 'perseus_test@arcadialab.ru'
-    SMTP_USER = 'perseus_test'
-    SMTP_PWD = 'PT4TYxou@?cH8J'
-    SERVER_HOST = '10.110.1.7'
-    EMAIL_ENCODE_KEY = 'DiTdwHZMCnfj4CzSV12RzC64H3SRp-iY-0euf2grz7s='
-
