5fdb9bd43f8fffb3f65b4c2f5cd8de5c41567164
Maria Dolotova <mdoloto1@its.jnj.com>
2021-09-29 19:16:30 +0300

    removed SMTP setings from config, updated README

--- cdm_souffleur/config.py
+++ cdm_souffleur/config.py
@@ -7,11 +7,6 @@ class DefaultConfig:
     DB_HOST = 'localhost'
     DB_PORT = 5431
     SECRET_KEY = "\xac\xe3\xc1\x8ft\x8d\x7fM\x82\x18\xbeM\xc7k*\xd5]\xae(J\xe6}\xe84"
-    SMTP_SERVER = ''
-    SMTP_PORT = 587
-    SMTP_EMAIL = ''
-    SMTP_USER = ''
-    SMTP_PWD = ''
     SERVER_HOST = 'localhost'
     EMAIL_ENCODE_KEY = 'DiTdwHZMCnfj4CzSV12RzC64H3SRp-iY-0euf2grz7s='
     SOLR_HOST = 'localhost'
@@ -26,11 +21,6 @@ class DevelopmentConfig:
     DB_HOST = 'localhost'
     DB_PORT = 5431
     SECRET_KEY = "\xac\xe3\xc1\x8ft\x8d\x7fM\x82\x18\xbeM\xc7k*\xd5]\xae(J\xe6}\xe84"
-    SMTP_SERVER = ''
-    SMTP_PORT = 587
-    SMTP_EMAIL = ''
-    SMTP_USER = ''
-    SMTP_PWD = ''
     SERVER_HOST = '10.110.1.7:8080'
     EMAIL_ENCODE_KEY = 'DiTdwHZMCnfj4CzSV12RzC64H3SRp-iY-0euf2grz7s='
     SOLR_HOST = 'localhost'
@@ -46,11 +36,6 @@ class StagingConfig:
     DB_HOST = 'localhost'
     DB_PORT = 5431
     SECRET_KEY = "\xac\xe3\xc1\x8ft\x8d\x7fM\x82\x18\xbeM\xc7k*\xd5]\xae(J\xe6}\xe84"
-    SMTP_SERVER = ''
-    SMTP_PORT = 587
-    SMTP_EMAIL = ''
-    SMTP_USER = ''
-    SMTP_PWD = ''
     SERVER_HOST = '10.110.1.7'
     EMAIL_ENCODE_KEY = 'DiTdwHZMCnfj4CzSV12RzC64H3SRp-iY-0euf2grz7s='
     SOLR_HOST = 'localhost'
@@ -65,11 +50,6 @@ class ProdConfig:
     DB_HOST = 'localhost'
     DB_PORT = 5431
     SECRET_KEY = "\xac\xe3\xc1\x8ft\x8d\x7fM\x82\x18\xbeM\xc7k*\xd5]\xae(J\xe6}\xe84"
-    SMTP_SERVER = ''
-    SMTP_PORT = 587
-    SMTP_EMAIL = ''
-    SMTP_USER = ''
-    SMTP_PWD = ''
     SERVER_HOST = '185.134.75.47'
     EMAIL_ENCODE_KEY = 'DiTdwHZMCnfj4CzSV12RzC64H3SRp-iY-0euf2grz7s='
     SOLR_HOST = 'localhost'
