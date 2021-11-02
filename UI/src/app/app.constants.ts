9b783e0a6dfb44e9198731be9012fa320a6256c2
Matvey Chudakov <matvey159@gmail.com>
2021-11-02 15:30:54 +0300

    fixed front config, changed sockjs version to 1.0.0

--- UI/src/app/app.constants.ts
+++ UI/src/app/app.constants.ts
@@ -1,41 +1,43 @@
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment'
 
 export const appVersion = '0.3'
 
-export const similarTableName = 'similar';
+export const similarTableName = 'similar'
 
-export const isProd = environment.production;
+export const isProd = environment.production
 export const isDev = !isProd;
 export const isLocal = environment.local
 
-export const dbServer = environment.dbServer;
+export const dbServer = environment.dbServer
 
 // urls
-export const server = environment.server;
+export const server = environment.server
 
 export const serverUrl = environment.port === 80 ?
   `http://${server}` :
-  `http://${server}:${environment.port}`;
+  `http://${server}:${environment.port}`
 
-export const apiUrl = `${serverUrl}/api`;
+export const apiUrl = `${serverUrl}/api`
 
-export const whiteRabbitPrefix = '/white-rabbit-service';
-export const whiteRabbitWsUrl = `${serverUrl}${whiteRabbitPrefix}`
-export const whiteRabbitApiUrl = `${serverUrl}${whiteRabbitPrefix}/api`;
+export const whiteRabbitServerUrl = serverUrl
+export const whiteRabbitPrefix = '/white-rabbit-service'
+export const whiteRabbitWsUrl = `${whiteRabbitServerUrl}${whiteRabbitPrefix}`
+export const whiteRabbitApiUrl = `${whiteRabbitServerUrl}${whiteRabbitPrefix}/api`
 
-export const cdmBuilderLogUrl = `${serverUrl}/log`;
-export const cdmBuilderApiUrl = `${serverUrl}/cdm-builder/api`;
+export const cdmBuilderServerUrl = serverUrl
+export const cdmBuilderLogUrl = `${cdmBuilderServerUrl}/log`
+export const cdmBuilderApiUrl = `${cdmBuilderServerUrl}/cdm-builder/api`
 
-export const dqdUrl = `${serverUrl}/dqd`;
-export const dqdApiUrl = `${dqdUrl}/api`;
-export const dqdWsUrl = `ws://${server}:8001/dqd/progress`; // 8001 port
+export const dqdUrl = `${serverUrl}/dqd`
+export const dqdApiUrl = `${dqdUrl}/api`
+export const dqdWsUrl = `ws://${server}:8001/dqd/progress` // 8001 port
 //
 
-export const numberOfPanelsWithoutSimilar = 2;
-export const numberOfPanelsWithOneSimilar = 3;
-export const numberOfPanelsWithTwoSimilar = 4;
+export const numberOfPanelsWithoutSimilar = 2
+export const numberOfPanelsWithOneSimilar = 3
+export const numberOfPanelsWithTwoSimilar = 4
 
-export const conceptFieldsTypes = ['concept_id', 'source_value', 'source_concept_id', 'type_concept_id'];
+export const conceptFieldsTypes = ['concept_id', 'source_value', 'source_concept_id', 'type_concept_id']
 
 export const mainPageRouter = '/perseus'
 export const loginRouter = '/sign-in'
@@ -61,4 +63,5 @@ export const COLUMNS_TO_EXCLUDE_FROM_TARGET = [
   'CONCEPT_SYNONYM',
   'CONCEPT_ANCESTOR',
   'SOURCE_TO_CONCEPT_MAP',
-  'DRUG_STRENGTH' ];
+  'DRUG_STRENGTH'
+]
