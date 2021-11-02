13b5965d703461061b4019cde100117b2762874b
Matvey Chudakov <matvey159@gmail.com>
2021-11-02 13:46:34 +0300

    improved load_csv script, added default vocabulary

--- database/load_csv.sh
+++ database/load_csv.sh
@@ -2,13 +2,28 @@ apt-get update -y
 apt-get install unzip wget -y
 
 cd tmp
-wget -O vocabulary.zip $VOC_URL
 
-# Archive not empty
-if zipinfo vocabulary.zip > /dev/null; then
-	unzip 'vocabulary.zip' -d vocabulary
+if [ -z "$VOC_URL" ]; then
+  echo "Using default vocabulary"
+  sleep 1
+  unzip 'vocabulary.zip' -d vocabulary
 	rm vocabulary.zip
 else
-	echo "Vocabulary link incorrect or expired"
-	exit 1
+  rm vocabulary.zip
+  echo "Downloading vocabulary by link..."
+  sleep 1
+  wget -O vocabulary.zip "$VOC_URL"
+
+  # Archive not empty
+  if zipinfo vocabulary.zip > /dev/null; then
+    echo "Successfully downloaded"
+    echo "Unzipping..."
+    sleep 1
+    unzip 'vocabulary.zip' -d vocabulary
+    rm vocabulary.zip
+    echo "Vocabulary successfully downloaded and unzipped"
+  else
+    echo "Vocabulary link incorrect or expired"
+    exit 1
+  fi
 fi
