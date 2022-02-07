8f4e46fe49c8ca489ecbb738425bb7a1cba8c219
Matvey Chudakov <matvey159@gmail.com>
2022-02-07 11:25:38 +0300

    Vocabulary-search (Athena) microservice

---
+++ vocabulary-search/constants.py
@@ -0,0 +1,9 @@
+VOCABULARY_FILTERS = {
+    'concept_class_id': 'conceptClass',
+    'domain_id': 'domain',
+    'invalid_reason': 'invalidReason',
+    'standard_concept': 'standardConcept',
+    'vocabulary_id': 'vocabulary',
+}
+
+ATHENA_CORE_NAME = 'athena'
