7dc0322815d729107f2bf5ce1599f5e36c21b9f6
Matvey Chudakov <matvey159@gmail.com>
2022-04-28 13:47:55 +0300

    Implemented clear-cache job

--- perseus-api/main.py
+++ perseus-api/main.py
@@ -4,6 +4,7 @@ from app import app
 from config import PORT
 from create_tables import create_tables
 from perseus_api import perseus
+from services.clear_cache_job import create_clear_cache_job
 from utils import UPLOAD_SCAN_REPORT_FOLDER
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_SCAN_REPORT_FOLDER
@@ -11,4 +12,5 @@ app.register_blueprint(perseus)
 
 if __name__ == '__main__':
     create_tables()
+    create_clear_cache_job()
     serve(app, host='0.0.0.0', port=PORT)
