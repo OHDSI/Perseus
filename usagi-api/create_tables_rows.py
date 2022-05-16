65993b63fdca91bd038862d3a74a34c8f0adb7f0
LadyCaster <daria.shpak@arcadia.spb.ru>
2022-05-17 01:37:23 +0300

    USAGI: refactor db rows creation

--- usagi-api/create_tables_rows.py
+++ usagi-api/create_tables_rows.py
@@ -1,4 +1,7 @@
+import pandas as pd
 from peewee import fn
+
+from db_engines import vocabulary_engine, usagi_engine
 
 from model.usagi.atc_to_rxnorm import atc_to_rxnorm
 from model.usagi.child import Child_Count
@@ -11,6 +14,8 @@ from model.vocabulary.concept_vocabulary_model import Concept, Concept_Relations
 from util.constants import INSERT_BATCH_SIZE
 from util.usagi_db import usagi_pg_db
 
+
+valid_concept_records = []
 
 def create_rows_for_tables():
     create_valid_concept_ids()
@@ -29,8 +34,10 @@ def create_rows_for_tables():
 
 
 def create_valid_concept_ids():
+    records = Concept.select(Concept.concept_id).where(Concept.invalid_reason.is_null(True)).dicts()
+    global valid_concept_records
+    valid_concept_records = [record["concept_id"] for record in records]
     if Valid_Concept_Ids.select().count() == 0:
-        records = Concept.select(Concept.concept_id).where(Concept.invalid_reason.is_null(True)).dicts()
         with usagi_pg_db.atomic():
             for idx in range(0, len(records), INSERT_BATCH_SIZE):
                 rows = records[idx:idx + INSERT_BATCH_SIZE]
@@ -54,10 +61,10 @@ def create_maps_to_relationship():
                 Concept_Relationship.invalid_reason.is_null(True)
             ) & (
                 Concept_Relationship.concept_id_1!=Concept_Relationship.concept_id_2
+             ) & (
+                Concept_Relationship.concept_id_1.in_(valid_concept_records)
             ) & (
-                fn.EXISTS(Valid_Concept_Ids.select(Valid_Concept_Ids.concept_id).where(Valid_Concept_Ids.concept_id==Concept_Relationship.concept_id_1))
-            ) & (
-                fn.EXISTS(Valid_Concept_Ids.select(Valid_Concept_Ids.concept_id).where(Valid_Concept_Ids.concept_id==Concept_Relationship.concept_id_2))
+                Concept_Relationship.concept_id_2.in_(valid_concept_records)
             )
         ).dicts()
         with usagi_pg_db.atomic():
@@ -77,9 +84,9 @@ def create_relationship_atc_rxnorm():
             ) & (
                 Concept_Relationship.concept_id_1!=Concept_Relationship.concept_id_2
             ) & (
-                fn.EXISTS(Valid_Concept_Ids.select(Valid_Concept_Ids.concept_id).where(Valid_Concept_Ids.concept_id==Concept_Relationship.concept_id_1))
+                Concept_Relationship.concept_id_1.in_(valid_concept_records)
             ) & (
-                fn.EXISTS(Valid_Concept_Ids.select(Valid_Concept_Ids.concept_id).where(Valid_Concept_Ids.concept_id==Concept_Relationship.concept_id_2))
+                Concept_Relationship.concept_id_2.in_(valid_concept_records)
             )
         ).dicts()
         with usagi_pg_db.atomic():
@@ -112,9 +119,9 @@ def create_parent_child_relationship():
             ) & (
                 Concept_Ancestor.ancestor_concept_id!=Concept_Ancestor.descendant_concept_id
             ) & (
-                fn.EXISTS(Valid_Concept_Ids.select().where(Valid_Concept_Ids.concept_id==Concept_Ancestor.ancestor_concept_id))
+                Concept_Ancestor.ancestor_concept_id.in_(valid_concept_records)
             ) & (
-                fn.EXISTS(Valid_Concept_Ids.select().where(Valid_Concept_Ids.concept_id==Concept_Ancestor.descendant_concept_id))
+                Concept_Ancestor.descendant_concept_id.in_(valid_concept_records)
             )
         ).dicts()
         with usagi_pg_db.atomic():
@@ -149,29 +156,16 @@ def create_child_count():
 
 def create_usagi_concept():
     if UConcept.select().count() == 0:
-        records = Concept.select(
-            Concept.concept_id,
-            Concept.concept_name,
-            Concept.domain_id,
-            Concept.vocabulary_id,
-            Concept.concept_class_id,
-            Concept.standard_concept,
-            Concept.concept_code,
-            Concept.valid_start_date,
-            Concept.valid_end_date,
-            Concept.invalid_reason,
-            Parent_Count.parent_count,
-            Child_Count.child_count
-        ).join(
-            Parent_Count, on=(Parent_Count.descendant_concept_id==Concept.concept_id)
-        ).join(
-            Child_Count, on=(Child_Count.ancestor_concept_id==Concept.concept_id)
-        ).dicts()
-
-        with usagi_pg_db.atomic():
-            for idx in range(0, len(records), INSERT_BATCH_SIZE):
-                rows = records[idx:idx + INSERT_BATCH_SIZE]
-                UConcept.insert_many(rows).execute()
+        parent_count_records = pd.read_sql_table('parent_count', usagi_engine, 'usagi')
+        child_count_records = pd.read_sql_table('child_count', usagi_engine, 'usagi')
+        vconcept_records = pd.read_sql_table('concept', vocabulary_engine, 'vocabulary')
+        records = vconcept_records.merge(parent_count_records, how='outer', left_on='concept_id', right_on='descendant_concept_id')
+        parent_count_records = None
+        vconcept_records = None
+        records = records.merge(child_count_records, how='outer', left_on='concept_id', right_on='ancestor_concept_id')
+        child_count_records = None
+        records.drop(['id_x', 'id_y', 'descendant_concept_id', 'ancestor_concept_id'], axis=1, inplace=True)
+        records.to_sql('concept', usagi_engine, 'usagi', chunksize=1000, index=False, if_exists='append')
 
 
 def create_concept_for_index():
@@ -220,25 +214,27 @@ def create_concept_for_index_3():
         Concept_For_Index.standard_concept.in_(('S', 'C'))) & (
         fn.LOWER(Concept_For_Index.term)!=fn.LOWER(UConcept.concept_name))
     ).count() == 0:
-        t1 = UConcept.alias()
-        t2 = Concept_Synonym.alias()
-        records =  t1.select(
-            t2.concept_synonym_name.alias('term'), t1.concept_id,
-            t1.domain_id, t1.vocabulary_id,
-            t1.concept_class_id, t1.standard_concept
-        ).join(
-            t2, on=(t1.concept_id==t2.concept_id)
-        ).where(
-            (
-                t1.standard_concept.in_(('S', 'C'))
-            ) & (
-                fn.LOWER(t1.concept_name)!=fn.LOWER(t2.concept_synonym_name)
-            )
-        ).distinct().dicts()
-        with usagi_pg_db.atomic():
-            for idx in range(0, len(records), INSERT_BATCH_SIZE):
-                rows = records[idx:idx + INSERT_BATCH_SIZE]
-                Concept_For_Index.insert_many([{"type": 'C', "term_type": 'C', **row} for row in rows]).execute()
+        concept_synonym_records = pd.read_sql_table('concept_synonym', vocabulary_engine, 'vocabulary')
+        uconcept_records = pd.read_sql_table('concept', usagi_engine, 'usagi')
+        records = uconcept_records.merge(concept_synonym_records, how='inner', on='concept_id')
+        concept_synonym_records = None
+        uconcept_records = None
+        records = records.loc[(records['concept_name'].str.lower() != records['concept_synonym_name'].str.lower()) &
+              records['standard_concept'].isin(['S', 'C'])]
+        records.drop([
+            'parent_count',
+            'child_count',
+            'concept_code',
+            'invalid_reason',
+            'valid_end_date',
+            'valid_start_date',
+            'concept_name',
+            'language_concept_id'
+            ], axis=1, inplace=True)
+        records.rename(columns={'concept_synonym_name': 'term'}, inplace=True)
+        records["type"] = "C"
+        records["term_type"] = "C"
+        records.to_sql('concept_for_index', usagi_engine, 'usagi', chunksize=1000, index=False, if_exists='append')
 
 
 def create_concept_for_index_4():
@@ -250,28 +246,36 @@ def create_concept_for_index_4():
         UConcept.standard_concept.is_null(True)) & (
         fn.LOWER(Concept_For_Index.term)!=fn.LOWER(UConcept.concept_name))
     ).count() == 0:
-        t1 = UConcept.alias()
-        t2 = Concept_Synonym.alias()
-        t3 = Maps_To_Relationship.alias()
-        t4 = UConcept.alias()
-        records = t1.select(
-            t2.concept_synonym_name.alias('term'), t4.concept_id,
-            t4.domain_id, t4.vocabulary_id,
-            t4.concept_class_id, t4.standard_concept
-        ).join(
-            t2, on=(t1.concept_id==t2.concept_id)
-        ).join(
-            t3, on=(t1.concept_id==t3.concept_id_1)
-        ).join(
-            t4, on=(t3.concept_id_2==t4.concept_id)
-        ).where(
-            (
-                t1.standard_concept.is_null(True)
-            ) & (
-                fn.LOWER(t2.concept_synonym_name)!=fn.LOWER(t4.concept_name)
-            )
-        ).distinct().dicts()
-        with usagi_pg_db.atomic():
-            for idx in range(0, len(records), INSERT_BATCH_SIZE):
-                rows = records[idx:idx + INSERT_BATCH_SIZE]
-                Concept_For_Index.insert_many([{"type": 'C', "term_type": 'S', **row} for row in rows]).execute()
+        concept_synonym_records = pd.read_sql_table('concept_synonym', vocabulary_engine, 'vocabulary')
+        uconcept_records_1 = pd.read_sql_table('concept', usagi_engine, 'usagi', columns=['standard_concept', 'concept_id'])
+        uconcept_records_1.rename(columns={'standard_concept': 'standard_concept_1'}, inplace=True)
+        records = uconcept_records_1.merge(concept_synonym_records, how='inner', on='concept_id')
+        concept_synonym_records = None
+        uconcept_records_1 = None
+        maps_to_rels_records = pd.read_sql_table('maps_to_relationship', usagi_engine, 'usagi')
+        records = records.merge(maps_to_rels_records, how='inner', left_on='concept_id', right_on='concept_id_1')
+        maps_to_rels_records = None
+        uconcept_records_2 = pd.read_sql_table('concept', usagi_engine, 'usagi', columns=[
+                                                                                         'concept_id',
+                                                                                         'concept_name',
+                                                                                         'domain_id',
+                                                                                         'vocabulary_id',
+                                                                                         'concept_class_id',
+                                                                                         'standard_concept'
+                                                                                         ])
+        records = records.merge(uconcept_records_2, how='inner', left_on='concept_id_2', right_on='concept_id')
+        records = records.loc[(records['concept_name'].str.lower() != records['concept_synonym_name'].str.lower()) &
+              records['standard_concept_1'].isna()]
+        records.drop([
+            'id',
+            'standard_concept_1',
+            'language_concept_id',
+            'concept_id_1',
+            'concept_id_2',
+            'concept_id_x',
+            'concept_name'
+            ], axis=1, inplace=True)
+        records.rename(columns={'concept_synonym_name': 'term', 'concept_id_y': 'concept_id'}, inplace=True)
+        records["type"] = "C"
+        records["term_type"] = "S"
+        records.to_sql('concept_for_index', usagi_engine, 'usagi', chunksize=1000, index=False, if_exists='append')
