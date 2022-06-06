6a9a7c1042917f74a29af05e35913f1bddf1f4ea
Matvey Chudakov <matvey159@gmail.com>
2022-06-06 19:38:35 +0300

    Perseus api: key vault integration

--- perseus-api/app.py
+++ perseus-api/app.py
@@ -1,12 +1,10 @@
-import os
 import logging
 from flask import Flask
 from flask_cors import CORS
-
+from app_config import init_app_config
 
 app = Flask(__name__)
-env = os.getenv("PERSEUS_ENV").capitalize()
-app.config.from_object(f'config.{env}Config')
+init_app_config(app)
 CORS(app)
 logger = logging.getLogger('waitress')
 logger.setLevel(logging.INFO)
