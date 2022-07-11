2a989dc954caf182638259df6e75ae2afffc6a24
Matvey Chudakov <matvey159@gmail.com>
2022-07-11 18:42:21 +0300

    Solr: usagi integration

--- solr/entrypoint.sh
+++ solr/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 service ssh start
-runuser -u solr -- solr-precreate athena /opt/solr/server/solr/configsets/athena/conf
+runuser -u solr -- precreate-core athena /opt/solr/server/solr/configsets/athena/conf
+runuser -u solr -- solr-precreate usagi /opt/solr/server/solr/configsets/usagi/conf
