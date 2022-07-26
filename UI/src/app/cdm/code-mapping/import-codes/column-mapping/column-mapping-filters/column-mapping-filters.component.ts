0c160c7e15e3a75f38840d537707840a70eaab6d
Anton Ivanov <antoha.ivanov@gmail.com>
2022-07-26 13:41:59 +0300

    Auto check filters' group if at least one filter selected

--- UI/src/app/cdm/code-mapping/import-codes/column-mapping/column-mapping-filters/column-mapping-filters.component.ts
+++ UI/src/app/cdm/code-mapping/import-codes/column-mapping/column-mapping-filters/column-mapping-filters.component.ts
@@ -3,6 +3,7 @@ import { FormGroup } from '@angular/forms';
 import { getFilters } from '@models/code-mapping/filters';
 import { ImportCodesService } from '@services/usagi/import-codes.service';
 import { Filter } from '@models/filter/filter';
+import { SearchConceptFilters } from '@app/models/code-mapping/search-concept-filters';
 
 @Component({
   selector: 'app-column-mapping-filters',
@@ -30,5 +31,13 @@ export class ColumnMappingFiltersComponent implements OnInit {
           this.dropdownFilters.find(filter => filter.field === key).values = result[key]
         )
       })
+  }
+
+  onChangeValue(value: string) {
+    const searchConceptFilters = this.form.value as SearchConceptFilters;
+
+    this.form.get('filterByConceptClass').setValue(searchConceptFilters.conceptClasses.length > 0)
+    this.form.get('filterByVocabulary').setValue(searchConceptFilters.vocabularies.length > 0)
+    this.form.get('filterByDomain').setValue(searchConceptFilters.domains.length > 0)
   }
 }
