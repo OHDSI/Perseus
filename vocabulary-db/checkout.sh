537821013848fca317b3c5e44fcf5852a9e8189f
Aleksei Kostiushenko <aleksei.kostiushenko@gmail.com>
2022-09-21 04:21:28 +0300

    Add checkout script for vocabulary dictionary files

---
+++ vocabulary-db/checkout.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PAT=$1
+/bin/echo ${PAT}
+PAT64=`/bin/echo ${PAT} | base64`
+/bin/echo ${PAT64}
+
+git -c http.extraHeader="Authorization: Basic ${PAT64}" clone https://dev.arcadia.spb.ru/Arcadia-CDM/Perseus/_git/vocabulary
