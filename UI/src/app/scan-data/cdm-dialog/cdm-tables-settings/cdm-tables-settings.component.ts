f31e060c3a8b843d9b3d0be9d90fc6cc7e5bc582
Eugene Zubkov <eugene.zubkov@softwarecountry.com>
2023-01-17 14:11:28 +0400

    add search

--- UI/src/app/scan-data/cdm-dialog/cdm-tables-settings/cdm-tables-settings.component.ts
+++ UI/src/app/scan-data/cdm-dialog/cdm-tables-settings/cdm-tables-settings.component.ts
@@ -7,6 +7,9 @@ import {
     ViewChild, 
     Renderer2
 } from "@angular/core";
+import { StoreService } from '@app/services/store.service';
+import { ITable } from '@app/models/table';
+import { Criteria } from '@shared/search-by-name/search-by-name.component';
 
 @Component({
     selector: 'app-cdm-tables-settings',
@@ -20,15 +23,36 @@ export class CdmTablesSettingsComponent extends BaseComponent implements OnInit,
     private handleClickPopup: () => void;
 
     public showSettings = false;
+    public readonly popupTitle = 'Settings for Tables';
+    public readonly searchPlaceholder = 'Search Tables';
+    public query: string = '';
+    
+    private initTables: ITable[] = [];
+    public filteredTables: ITable[] = [];    
 
-    constructor(private renderer: Renderer2) {
+    constructor(private renderer: Renderer2, private storeService: StoreService) {
         super();
+    }
+
+    filterByName(byName: Criteria): void {
+        this.query = byName.criteria;
+        this.filteredTables = this.initTables.filter(item => item.name.includes(byName.criteria));
+    }
+
+    resetFilter(): void {
+        this.query = '';
+        this.filteredTables = [...this.initTables];
     }
 
 
     ngOnInit(): void {
+        this.initializeTablesData();
+        
+        // TODO: possibly refactor with host listener ?
         this.handleClickPopup = this.renderer.listen('document', 'click', event => {
-            const clickedInside = this.settingsEl.nativeElement.contains(event.target);
+            const clickedPopup = this.settingsEl.nativeElement.contains(event.target);
+            const resetSearch = event.target.classList.contains('mat-icon');
+            const clickedInside = clickedPopup || resetSearch;
             if (!clickedInside) {
                 this.showSettings = false;
                 return;
@@ -36,8 +60,13 @@ export class CdmTablesSettingsComponent extends BaseComponent implements OnInit,
 
             const clickedBtn = this.settingsBtn.nativeElement.contains(event.target);
             this.showSettings = clickedBtn ? !this.showSettings : this.showSettings;
-            
         });
+    }
+
+    private initializeTablesData(): void {
+        const { target } = this.storeService.state;
+        this.initTables = [...target];
+        this.filteredTables = [...this.initTables]; 
     }
 
     ngOnDestroy(): void {
